b59741264e745ec24be114a074bfde1c4f4d5f3d
NguyenXuanVinh <nongami87@gmail.com>
2025-09-12 18:17:59 +0700

    feat: integrate Sequelize ORM and initial DB setup
    - introduces Sequelize ORM and CLI for database management, adds environment variable samples, and sets up database configuration for multiple environments.
    - includes middleware for DB authentication and routes loading structure to enable scalable resource handling.
    - facilitates future database integration, migrations, and model organization.

--- server.ts
+++ server.ts
@@ -4,8 +4,11 @@ require("module-alias/register");
 const express = require("express");
 const cors = require("cors");
 
+const mainRouter = require("@/routes");
+
 const response = require("@/middlewares/response.middleware");
 const handleError = require("@/middlewares/handle-error.middleware");
+const sequelizeAuthenticate = require("@/middlewares/sequelize-authenticate.middleware");
 
 const app = express();
 const port = 3001;
@@ -19,6 +22,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.use("/api/v1", sequelizeAuthenticate, mainRouter);
 app.use(handleError);
 
 app.listen(port, () => {
